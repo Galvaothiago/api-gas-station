ea904108c10b89212dcdef957d69fb45972a29f1
Thiago Galvao <thiagosilva128@gmail.com>
2021-11-28 18:44:19 -0300

    created initial project

---
+++ src/main/java/com/gasstation/api/GasstationapiApplication.java
@@ -0,0 +1,13 @@
+package com.gasstation.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasstationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasstationapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gasstation/api/GasstationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasstation.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasstationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
