613c93c1be1afbe39d8af711a5b49c40eeb1d4d5
Thiago Galvao <thiagosilva128@gmail.com>
2022-01-01 15:00:24 -0300

    populating the database with more information about gasStation

--- src/main/java/com/gasstation/api/config/TestConfig.java
+++ src/main/java/com/gasstation/api/config/TestConfig.java
@@ -20,51 +20,60 @@ import com.gasstation.api.repositories.PriceRepository;
 public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
-	private PriceRepository priceRepository;
-	
-	@Autowired
 	private GasStationRepository gasStationRepository;
-	
-	@Autowired
-	private AddressRepository addressRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {		
 	
-//		GasPrice price2 = new GasPrice(null, 6.52, 4.79, 4.55, 6.35, 5.50);
-//		GasPrice price3 = new GasPrice(null, 6.30, 4.80, 4.70, 6.40, 5.20);
-//		GasPrice price4 = new GasPrice(null, 6.52, 4.79, 4.55, 6.35, 5.50);
-//		GasPrice price5 = new GasPrice(null, 6.30, 4.80, 4.70, 6.40, 5.20);
-//		GasPrice price6 = new GasPrice(null, 6.52, 4.79, 4.55, 6.35, 5.50);
-//		
-		
 		GasStation company1 = new GasStation(null, "Posto ATMA", "https://amazons3.com/fdgkgjdfhdflfn");
-		
+		GasStation company2 = new GasStation(null, "Posto CAPELA", "https://amazons3.com/DFGNIFNBSDF46");
+		GasStation company3 = new GasStation(null, "Posto Estrela", "https://amazons3.com/fdgkgjdfhdflfn");
+		GasStation company4 = new GasStation(null, "Posto nova palmares", "https://amazons3.com/DFGNIFNBSDF46");
+		GasStation company5 = new GasStation(null, "Posto Preço bom", "https://amazons3.com/fdgkgjdfhdflfn");
+		GasStation company6 = new GasStation(null, "Posto praça 2", "https://amazons3.com/DFGNIFNBSDF46");
+//		
+					
 		Address address1 = new Address(null, "Rua maria pereira", "112", "Vinhedo", company1);
+		Address address2 = new Address(null, "Rua Vinhedo", "112", "Vinhedo", company2);
+		Address address3 = new Address(null, "Avenida Independencia", "2135", "Vinhedo", company3);
+		Address address4 = new Address(null, "Rua Regina Giunco", "778", "Vinhedo", company4);
+		Address address5 = new Address(null, "Rua Osasco 2", "12", "Vinhedo", company5);
+		Address address6 = new Address(null, "Avenida Rufino norte", "889", "Vinhedo", company6);
+		
 		GasPrice price1 = new GasPrice(null, 6.30, 4.80, 4.70, 6.40, 5.20, company1, Instant.now());
+		GasPrice price2 = new GasPrice(null, 6.52, 4.79, 4.55, 6.35, 5.50, company2, Instant.now());
+		GasPrice price3 = new GasPrice(null, 6.30, 4.80, 4.70, 6.40, 5.20, company3, Instant.now());
+		GasPrice price4 = new GasPrice(null, 6.52, 4.79, 4.55, 6.35, 5.50, company4, Instant.now());
+		GasPrice price5 = new GasPrice(null, 6.30, 4.80, 4.70, 6.40, 5.20, company5, Instant.now());
+		GasPrice price6 = new GasPrice(null, 6.52, 4.79, 4.55, 6.35, 5.50, company6, Instant.now());
 		
 		company1.setAddress(address1);
 		company1.setPrices(price1);
 		
+		company2.setAddress(address2);
+		company2.setPrices(price2);
 		
-//		Address address2 = new Address(null, "Rua Vinhedo", "112", "Vinhedo");
-//		Address address3 = new Address(null, "Avenida Independencia", "2135", "Vinhedo");
-//		Address address4 = new Address(null, "Rua Regina Giunco", "778", "Vinhedo");
-//		Address address5 = new Address(null, "Rua Osasco 2", "12", "Vinhedo");
-//		Address address6 = new Address(null, "Avenida Rufino norte", "889", "Vinhedo");
+		company3.setAddress(address3);
+		company3.setPrices(price3);
+		
+		company4.setAddress(address4);
+		company4.setPrices(price4);
+		
+		company5.setAddress(address5);
+		company5.setPrices(price5);
+		
+		company6.setAddress(address6);
+		company6.setPrices(price6);
+		
+		
+
 //		
 		
 
-//		GasStation company2 = new GasStation(null, "Posto CAPELA", "https://amazons3.com/DFGNIFNBSDF46", price1, address2);
-//		GasStation company3 = new GasStation(null, "Posto Estrela", "https://amazons3.com/fdgkgjdfhdflfn", price4, address4);
-//		GasStation company4 = new GasStation(null, "Posto nova palmares", "https://amazons3.com/DFGNIFNBSDF46", price5, address1);
-//		GasStation company5 = new GasStation(null, "Posto Preço bom", "https://amazons3.com/fdgkgjdfhdflfn", price3, address6);
-//		GasStation company6 = new GasStation(null, "Posto praça 2", "https://amazons3.com/DFGNIFNBSDF46", price6, address5);
 //		
-		
 //		priceRepository.saveAll(Arrays.asList(price1, price2, price3, price4, price5, price6));
 //		addressRepository.saveAll(Arrays.asList(address1));
-		gasStationRepository.saveAll(Arrays.asList(company1));
+		gasStationRepository.saveAll(Arrays.asList(company1, company2, company3, company4, company5, company6));
 	}
 
 }
