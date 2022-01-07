85b027cd83223c66c5ece91ec5d96b805543f139
Thiago Galvao <thiagosilva128@gmail.com>
2022-01-07 13:58:33 -0300

    service security

---
+++ src/main/java/com/gasstation/api/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.gasstation.api.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gasstation.api.model.auth.User;
+import com.gasstation.api.repositories.auth.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found with username " + username));
+		
+		return UserDetailsImpl.build(user);
+	}
+}
