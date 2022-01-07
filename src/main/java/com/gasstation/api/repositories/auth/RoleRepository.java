8682dd0ab6cb0c911c124f517c8d10834bdcc9ea
Thiago Galvao <thiagosilva128@gmail.com>
2022-01-07 09:53:38 -0300

    remove one option in ERole

--- src/main/java/com/gasstation/api/repositories/auth/RoleRepository.java
+++ src/main/java/com/gasstation/api/repositories/auth/RoleRepository.java
@@ -1,5 +1,12 @@
 package com.gasstation.api.repositories.auth;
 
-public interface RoleRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gasstation.api.model.auth.ERole;
+import com.gasstation.api.model.auth.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+	Optional<Role> findByName(ERole name);
 }
