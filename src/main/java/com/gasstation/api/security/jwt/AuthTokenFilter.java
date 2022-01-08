5deeb51b401f135921f5e4bdd7ae86fe7804b81c
Thiago Galvao <thiagosilva128@gmail.com>
2022-01-08 19:21:58 -0300

    updated AuthTokenFIlter

--- src/main/java/com/gasstation/api/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/gasstation/api/security/jwt/AuthTokenFilter.java
@@ -46,11 +46,10 @@ public class AuthTokenFilter extends OncePerRequestFilter{
 				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 				
 				SecurityContextHolder.getContext().setAuthentication(authentication);
-
 				
 			}
 		} catch (Exception e) {
-			logger.error("Cannot set user authentication: ", e.getCause());
+			logger.error("Cannot set user authentication: " + e.getMessage());
 		}
 		
 		filterChain.doFilter(request, response);
