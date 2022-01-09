3ff896beddcaae94a5089da9368df77f65ab5528
Thiago Galvao <thiagosilva128@gmail.com>
2022-01-09 16:43:31 -0300

    created a new endpoint, to show quantity city seaching by city

--- src/main/java/com/gasstation/api/repositories/AddressRepository.java
+++ src/main/java/com/gasstation/api/repositories/AddressRepository.java
@@ -1,9 +1,12 @@
 package com.gasstation.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.gasstation.api.model.CountingCity;
 import com.gasstation.api.model.entities.Address;
 
 public interface AddressRepository extends JpaRepository<Address, Long>{
-
+	@Query("select new com.gasstation.api.model.CountingCity(a.city, count(*)) from Address a where a.city = ?1 group by city")
+	public CountingCity findCountCity(String city);
 }
